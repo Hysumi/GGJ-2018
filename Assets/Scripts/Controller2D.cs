809bacd2383c82a1daff176fc19fb9a05f05e7c3
Henrique Sumitomo <hysumi@gmail.com>
2018-01-27 16:06:44 -0200

    SL-2D Platforer E06 (moving platform basic)

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -1,32 +1,18 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))] //Autimaticamente adiciona o BoxCollider2D ao objeto e não permite que retire
-
 /*
  * ref: Toda alteração feita em uma variável com ref, vai alterar seu valor nas demais funções, não apenas localmente
  */
-public class Controller2D : MonoBehaviour {
-
-    public LayerMask collisionMask; 
-
-    const float skinWidth = .015f;
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
+public class Controller2D : RaycastController {
 
     float maxClimbAngle = 80;
     float maxDescendAngle = 75;
-
-    float horizontalRaySpacing;
-    float verticalRaySpacing;
-
-    BoxCollider2D collider;
-    RaycastOrigins raycastOrigins;
+   
     public CollisionsInfo collisions;
 
-    private void Start()
+    public override void Start() //Chama o método Start do RaycastController
     {
-        collider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
+        base.Start();
     }
 
     //Função que controla o movimento
@@ -202,36 +188,6 @@ public class Controller2D : MonoBehaviour {
                 }
             }
         }
-    }
-
-    void UpdateRaycastOrigins() //A origem do raycast no objeto
-    {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2); //coloca para dentro de uma "pele"
-
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-
-    }
-
-    void CalculateRaySpacing() //Distância entre os raycasts
-    {
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-    }
-
-    struct RaycastOrigins //Corners do objeto
-    {
-        public Vector2 topLeft, topRight;
-        public Vector2 bottomLeft, bottomRight;
     }
 
     public struct CollisionsInfo
---
+++ Assets/Scripts/RaycastController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))] //Autimaticamente adiciona o BoxCollider2D ao objeto e não permite que retire
+public class RaycastController : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    public const float skinWidth = .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    [HideInInspector]
+    public float horizontalRaySpacing;
+    [HideInInspector]
+    public float verticalRaySpacing;
+
+    [HideInInspector]
+    public BoxCollider2D collider;
+    public RaycastOrigins raycastOrigins;
+
+    public virtual void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void UpdateRaycastOrigins() //A origem do raycast no objeto
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2); //coloca para dentro de uma "pele"
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+
+    }
+
+    public void CalculateRaySpacing() //Distância entre os raycasts
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public struct RaycastOrigins //Corners do objeto
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
